34afb26a1fde2106b61f90f0e04c90711b68cb38
Pjotr Prins <pjotr.public01@thebird.nl>
2013-10-21 15:33:45 +0200

    scripts

--- scripts/protocol.sh
+++ scripts/protocol.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
 
+echo -n "."
 ./bin/gwp/query species > output/species_clusters.txt 2>&1
+echo -n "."
 ./bin/gwp/query match > output/match.txt 2>&1
+echo -n "."
 ./bin/gwp/query match --is_pos_sel > output/match_pos_sel.txt 2>&1
+echo -n "."
 ./bin/gwp/query homolog > output/homolog.txt 2>&1
+echo -n "."
 ./bin/gwp/query homolog --species > output/homolog_species.txt 2>&1
+echo -n "."
 ./bin/gwp/query homolog --nr > output/homolog_nr.txt 2>&1
